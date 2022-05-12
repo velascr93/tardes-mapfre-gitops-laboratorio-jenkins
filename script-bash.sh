f45744f36bbeed0a1dee74b921a22bbb0b719142
velascr93 <velascr@mapfre.com>
2022-05-12 14:57:33 +0200

    New: Nuevo script

---
+++ script-bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#set -x
+
+read -p "Escribe tu nombre: " NOMBRE
+
+if [ $NOMBRE = "secreto" ]
+then
+  echo -n "Encontraste el truco!: el primer parámetro del script es: "
+  echo $1 | rev
+else
+  echo "Encantado de saludarte, $NOMBRE"
+fi
