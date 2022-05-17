a6b40705118de71ca735d015191945b2fb2c0d36
velascr93 <104436694+velascr93@users.noreply.github.com>
2022-05-17 14:21:38 +0200

    Update script-bash.sh

--- script-bash.sh
+++ script-bash.sh
@@ -6,7 +6,7 @@ read -p "Escribe tu nombre: " NOMBRE
 if [ $NOMBRE = "secreto" ]
 then
   echo -n "Encontraste el truco!: el primer parámetro del script es: "
-  echo $1 | rev
+  echo $1
 else
   echo "Encantado de saludarte, $NOMBRE"
 fi
